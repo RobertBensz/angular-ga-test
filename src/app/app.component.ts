23fb7e88fb14d2b9488bcad8dc014b19334670f6
Robert Bensz <RBensz@dtiq.com>
2025-04-10 11:23:08 +0200

    Added 2 page events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,22 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'angular-ga-test';
+
+  sendAnalyticsEvent(): void {
+    console.log('sendAnalyticsEvent');
+    gtag('event', 'button_click', {
+      event_category: 'engagement',
+      event_label: 'send_event_button',
+      value: 1,
+    });
+  }
+
+  sendConfigEvent(): void {
+    console.log('sendConfigEvent');
+    gtag('config', 'page_route', { page_path: 'test' });
+  }
 }
